3be7080bffb924a8a157946f8e9cca1dfc4db895
uxdiva <uxdiva@darasplace.com>
2024-03-31 18:13:11 -0700

    fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const yaml = require("js-yaml");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({"shadows/assets/": "assets/"});
+  eleventyConfig.addPassthroughCopy({"shadows/assets/img": "assets/img"});
 
   eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
 
